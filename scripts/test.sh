27233879e40fa10e55dbcd747ad1ed99b1a452a3
chogamy <gamy0315@daum.net>
2025-03-05 15:52:24 +0900

    수정: 스크립트

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,2 +1,2 @@
-# CUDA_VISIBLE_DEVICES=0 deepspeed main.py
+# CUDA_VISIBLE_DEVICES=0 deepspeed main.py --model_name Qwen/Qwen2.5-VL-3B-Instruct
 deepspeed main.py --model_name Qwen/Qwen2.5-VL-3B-Instruct
